fbb1bf0dfb8ad12b9b2e7f26885769879ed73105
Ethan Pletcher <ethan@Ethans-MacBook-Air.local>
2023-04-19 14:32:57 -0700

    init-slash

--- src/navigation/Navigation.tsx
+++ src/navigation/Navigation.tsx
@@ -1,10 +1,11 @@
 import {NavigationContainer} from '@react-navigation/native';
-import AuthStackNavigator from './navigators/AuthStackNavigator';
+import RNBootSplash from 'react-native-bootsplash';
 import StackNavigator from './navigators/StackNavigator';
 
 export default function Navigation() {
   return (
-    <NavigationContainer>
+    <NavigationContainer
+      onReady={() => RNBootSplash.hide({fade: true, duration: 500})}>
       <StackNavigator />
     </NavigationContainer>
   );
